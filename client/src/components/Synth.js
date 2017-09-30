9ca8f3f0fd8793bb5a9800bb9e5f94c062aba8f4
Erik <erikejan@hotmail.com>
2017-09-30 14:32:25 +0200

    Reroute successful auth and gather user info

--- client/src/components/Synth.js
+++ client/src/components/Synth.js
@@ -4,19 +4,31 @@ import ActionCreators from '../actions/'
 import { Link } from 'react-router-dom'
 
 class Synth extends Component {
+  componentDidMount() {
+    // params injected via react-router, dispatch injected via connect
+    const {dispatch, params} = this.props.match
+    const {accessToken, refreshToken} = params
+    console.log(accessToken)
+    this.props.setTokens({accessToken, refreshToken})
+    this.props.setMe()
+  }
+
   render() {
     console.log("Rendering synth")
+    console.log("This is the user name")
+    console.log(this.props.accessToken)
     return (
       <div>
-        <h2>In the synth</h2>
+        <h2>Hi {this.props.displayName}</h2>
       </div>
     )
   }
 }
 
-function mapStateToProps(reducer) {
+function mapStateToProps(state) {
   return {
-    exampleCounter: reducer.state.example.get('example_counter') 
+    accessToken: state.spotify.accessToken,
+    displayName: state.spotify.user.display_name
   }
 }
 
