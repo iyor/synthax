e4cb051f2f1f204e4034ab8fbb54ad32946e84ac
Erik <erikejan@hotmail.com>
2017-09-30 10:53:52 +0200

    Serve login screen through backend

---
+++ server/app.js
@@ -0,0 +1,24 @@
+const express      = require('express');
+const bodyParser   = require('body-parser');
+const cookieParser = require('cookie-parser');
+const path         = require('path');
+const logger       = require('morgan');
+const routes       = require('./routes');
+
+const port = process.env.PORT || 3000;
+
+// configure the express server
+const app = express();
+
+app.set('port', port);
+app.use(logger('dev'))
+  .use(cookieParser())
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: false }))
+  .use(express.static(path.resolve(__dirname, '../client/public')))
+  .use('/', routes);
+
+// Start her up, boys
+app.listen(app.get('port'), () => {
+  console.log('Express server listening on port ' + app.get('port'));
+});
